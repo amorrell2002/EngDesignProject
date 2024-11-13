22a75e23e91371191db8f2faabbdb65885a06b43
morrellalex <morrellalex8@gmail.com>
2024-11-12 21:48:09 -0500

    Inital commit

---
+++ sketch_nov12a/sketch_nov12a.ino
@@ -0,0 +1,51 @@
+int lights [6];
+
+#define MOD1PINBUT 1
+#define MOD1PINLED 2
+
+#define MOD2PINBUT 4
+#define MOD2PINLED 4
+
+#define MOD3PINBUT 4
+#define MOD3PINLED 4
+
+#define MOD4PINBUT 4
+#define MOD4PINLED 4
+
+
+
+int pins [18] = {MOD1PIN1, MOD1PIN2, MOD2PIN1, MOD2PIN2, MOD3PIN3};
+
+
+
+
+void setup()
+{
+
+}
+
+
+int main()
+{
+
+}
+
+void gameLoop()
+{
+    //Choose led's to light up
+    //RNG light picking
+    //Write new list to lights
+    writeLights();
+
+
+    //Listen for player input
+    
+}
+
+void writeLights()
+{
+    for(int i = 0; i < 6; i++)
+    {
+        
+    }
+}
